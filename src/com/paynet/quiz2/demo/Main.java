18e50fb4c69299d4fc22c9a469d29ba9d35f69c0
shahrul.sharudin <shahrul.sharudin@iceinsuretech.com>
2022-06-09 04:46:00 +0100

    refactor

--- src/com/paynet/quiz2/demo/Main.java
+++ src/com/paynet/quiz2/demo/Main.java
@@ -1,30 +1,27 @@
 package com.paynet.quiz2.demo;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
+    private static int runningTotal;
 
     public static void main(String[] args) {
         int[] num = new int[9];
-        int runningTotal = 0;
+        runningTotal = 0;
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter 9 numbers:\n");
-
-        for (int i = 0; i < num.length; i++) {
-            num[i] = scan.nextInt();
-        }
+        IntStream.range(0,9).forEach(i->num[i] = scan.nextInt());
         scan.close();
 
         for (int j = 0; j < num.length - 1; j += 2) {
-            int subTotal = add(num[j], num[j + 1]);
-            runningTotal = runningTotal + subTotal;
-            System.out.println(runningTotal + "\n");
-
+            add(num[j], num[j + 1]);
         }
     }
 
-    private static int add(int a, int b) {
-        return a + b;
+    private static void add(int a, int b) {
+        runningTotal=runningTotal+a+b;
+        System.out.println(runningTotal + "\n");
     }
 }
